9ec48c033e62b76c4a57b0bd77c3aa4ef921f0f3
Mark Lehr <mark.lehr@rcc.edu>
2021-03-29 19:25:33 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/MarkQuerySort_Class/build/Debug/Cygwin-Windows/Array1D.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/Array1D.o: Array1D.cpp Array1D.h Array.h
-
-Array1D.h:
-
-Array.h:
--- Class/MarkQuerySort_Class/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp Array1D.h Array.h
-
-Array1D.h:
-
-Array.h:
